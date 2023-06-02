4cb6da27a45c68630df67312eb50daa4db377fcd
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-06-02 11:59:11 +0100

    Update 2D_SWIN_UNETR.py

--- 2D_SWIN_UNETR.py
+++ 2D_SWIN_UNETR.py
@@ -14,3 +14,24 @@ from torchsummary import summary
 #model = model()
 model.to(device=DEVICE,dtype=torch.float)
 summary(model, (1, 224, 224))
+
+#from monai.networks.nets import UNETR
+#import  torch
+#model = UNETR(
+#        img_size=(256,256),
+#        in_channels=4,
+#        out_channels=3,
+#        feature_size=24,
+#        spatial_dims=2
+#    )
+
+# from monai.networks.nets import SwinUNETR
+# import  torch
+# model = SwinUNETR(
+#         img_size=(256,256),
+#         in_channels=3,
+#         out_channels=3,
+#         feature_size=24,
+#         use_checkpoint=False,spatial_dims=2
+#     )
+#model_1  = model 
